50d452fd684e966911e381bcc2aba129438f633c
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-03-12 02:22:52 +0900

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,10 @@
+const form = () => {
+  let name = document.querySelector('name');
+  let lastname = document.querySelector('lastname');
+  let number = document.querySelector('number');
+  let city = document.querySelector('city');
+  let date = document.querySelector('date');
+  let sex = document.querySelector('sex');
+
+  alert(name)
+}
