85402f0e5a9da8c8857004e52bb0f422ebf9d8af
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-03-12 10:39:12 +0900

    form fixed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Jobnavigator
 
 Our project "JobNavigator" solves the problem of career guidance for young people.
+
+DONE
+
+- login page
---
+++ js/search.js
@@ -0,0 +1,6 @@
+const search = () => {
+    let input = document.getElementById('input');
+    let btn = document.getElementById('btn');
+
+    window.location.href = '#' + input;
+}
